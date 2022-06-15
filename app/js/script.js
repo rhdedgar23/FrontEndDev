59448536f13c7b45c92fbb6611f46eade23e01d7
rhdedgar23 <96807850+rhdedgar23@users.noreply.github.com>
2022-06-14 19:47:36 -0500

    Created animations.scss. Finished the animated mobile menu.

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
-console.log("hello world!");
+const hamburgerButton= document.querySelector('.header');
+const overlay= document.querySelector('.overlay');
+
+hamburgerButton.addEventListener('click', function(){
+    
+    console.log('click hamburger');
+
+    if(hamburgerButton.classList.contains('open')){
+        hamburgerButton.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+    else{
+        hamburgerButton.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+    //classList lets us manipulate an element's class content attribute
+});

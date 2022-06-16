808e7a9c5edc1af9e7368c816a66023ccd1e1b55
rhdedgar23 <96807850+rhdedgar23@users.noreply.github.com>
2022-06-15 22:21:14 -0500

    Updated overlay menu animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,25 @@
-const hamburgerButton= document.querySelector('.header');
+const header= document.querySelector('.header');
+const hambugerButton= document.querySelector('#hamburgerButton');
 const overlay= document.querySelector('.overlay');
+const overlayMenu= document.querySelector('.header__menuWindow');
 
 hamburgerButton.addEventListener('click', function(){
     
     console.log('click hamburger');
-
-    if(hamburgerButton.classList.contains('open')){
-        hamburgerButton.classList.remove('open');
+9
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
         overlay.classList.remove('fade-in');
         overlay.classList.add('fade-out');
+        overlayMenu.classList.remove('fade-in');
+        overlayMenu.classList.add('fade-out');
     }
     else{
-        hamburgerButton.classList.add('open');
+        header.classList.add('open');
         overlay.classList.remove('fade-out');
         overlay.classList.add('fade-in');
+        overlayMenu.classList.remove('fade-out');
+        overlayMenu.classList.add('fade-in');
     }
     //classList lets us manipulate an element's class content attribute
 });

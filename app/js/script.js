4ddc0b2e10830d097aa2ab322a7cece2f9df4c0f
rhdedgar23 <96807850+rhdedgar23@users.noreply.github.com>
2022-06-17 01:26:36 -0500

    Created hero.scss. Started background image and text section.

--- app/js/script.js
+++ app/js/script.js
@@ -2,12 +2,14 @@ const header= document.querySelector('.header');
 const hambugerButton= document.querySelector('#hamburgerButton');
 const overlay= document.querySelector('.overlay');
 const overlayMenu= document.querySelector('.header__menuWindow');
+const body= document.querySelector('body');
 
 hamburgerButton.addEventListener('click', function(){
     
     console.log('click hamburger');
 9
     if(header.classList.contains('open')){
+        body.classList.remove('noScroll');
         header.classList.remove('open');
         overlay.classList.remove('fade-in');
         overlay.classList.add('fade-out');
@@ -15,6 +17,7 @@ hamburgerButton.addEventListener('click', function(){
         overlayMenu.classList.add('fade-out');
     }
     else{
+        body.classList.add('noScroll');
         header.classList.add('open');
         overlay.classList.remove('fade-out');
         overlay.classList.add('fade-in');
